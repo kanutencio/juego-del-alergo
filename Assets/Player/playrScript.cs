f8da9f34548eec763feb6da2322a34387d36965f
jajavirale32-design <jajavirale.32@gmail.com>
2026-05-27 15:41:51 -0500

    aaaaaa

--- Assets/Player/playrScript.cs
+++ Assets/Player/playrScript.cs
@@ -7,19 +7,32 @@ public class playrScript : MonoBehaviour
     [SerializeField] private float velocidad;
     [SerializeField] private float FuerzaSalto;
     private Rigidbody2D rigidBody;
-    bool MirandoDerecha;
+    private bool MirandoDerecha = true;
+
+    [Header("Deteccion de Suelo Nueva")]
     [SerializeField] bool enSuelo;
     [SerializeField] LayerMask CapaSuelo;
+    [SerializeField] private Vector2 dimensionesCaja = new Vector2(0.5f, 0.1f);
+    [SerializeField] private float desplazamientoY = -0.8f;
+    private bool ejecutandoCoyote;
 
+    // 1. VARIABLE NUEVA PARA TU ANIMATOR
+    private Animator animator;
 
     [Header("Sistema de disparos")]
     [SerializeField] private balasPooling BP;
     public float LaserOffSet;
-    public float TiempoEntreDisparos = 0.15f;
-    
+    private float cooldownDisparo;
+    [SerializeField] public float TiempoEntreDisparos = 0.15f;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+
+        // 2. BUSCAMOS EL ANIMATOR AL INICIAR
+        animator = GetComponent<Animator>();
+
+        cooldownDisparo = 0;
     }
 
     void Update()
@@ -29,14 +42,13 @@ public class playrScript : MonoBehaviour
         ControlarSalto();
     }
 
-
     private void disparo()
     {
-        if (TiempoEntreDisparos > 0)
+        // Eliminamos todo el sistema de temporizador (Time.deltaTime) 
+        // para que no haya que esperar absolutamente nada entre clics.
+
+        if (Input.GetKeyDown(KeyCode.V))
         {
-            TiempoEntreDisparos -= Time.deltaTime;
-        }
-        if (Input.GetKeyDown(KeyCode.V) && TiempoEntreDisparos < 0){
             GameObject Dis = BP.Aparicion();
 
             Vector3 direccion = MirandoDerecha ? Vector3.right : Vector3.left;
@@ -48,60 +60,73 @@ public class playrScript : MonoBehaviour
                 rbDisparo.linearVelocity = direccion * 15;
             }
 
-            if (!MirandoDerecha)
-            {
-                Dis.transform.localScale = new Vector3(-1, 1, 1);
-            }
-            else
-            {
-                Dis.transform.localScale = new Vector3(1, 1, 1);
-            }
-
-            TiempoEntreDisparos = 0.15f;
+            Dis.transform.localScale = new Vector3(MirandoDerecha ? 1 : -1, 1, 1);
         }
-        
     }
 
     void ProcesarMovimiento()
     {
-        float inputMovimiento = Input.GetAxis("Horizontal");
+        float inputMovimiento = Input.GetAxisRaw("Horizontal");
         rigidBody.linearVelocity = new Vector2(inputMovimiento * velocidad, rigidBody.linearVelocity.y);
         GestionarOrientacion(inputMovimiento);
+
+        // 3. ¡AQUÍ SE ACTIVA TU ANIMACIÓN!
+        if (animator != null)
+        {
+            // Si el input es diferente de 0, significa que se está moviendo -> true
+            // Si el input es 0, está quieto -> false
+            bool seEstaMoviendo = (inputMovimiento != 0);
+            animator.SetBool("corriendo", seEstaMoviendo);
+        }
     }
+
     void GestionarOrientacion(float inputMovimiento)
     {
-        if ((MirandoDerecha == true && inputMovimiento < 0) || (MirandoDerecha == false && inputMovimiento > 0))
+        if ((inputMovimiento > 0 && !MirandoDerecha) || (inputMovimiento < 0 && MirandoDerecha))
         {
             MirandoDerecha = !MirandoDerecha;
-            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            Vector3 escala = transform.localScale;
+            escala.x *= -1;
+            transform.localScale = escala;
         }
     }
+
     void ControlarSalto()
     {
-        RaycastHit2D raycastSueloCentro = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, 0.4f, CapaSuelo);
-        RaycastHit2D raycastSueloIzquierda = Physics2D.Raycast(new Vector2(transform.position.x + 0.25f, transform.position.y), Vector2.down, 0.4f, CapaSuelo);
-        RaycastHit2D raycastSueloDerecha = Physics2D.Raycast(new Vector2(transform.position.x - 0.25f, transform.position.y), Vector2.down, 0.4f, CapaSuelo);
+        Vector2 posicionCaja = (Vector2)transform.position + new Vector2(0, desplazamientoY);
+        Collider2D colisionSuelo = Physics2D.OverlapBox(posicionCaja, dimensionesCaja, 0f, CapaSuelo);
 
-
-
-        if (raycastSueloCentro.collider != null || raycastSueloIzquierda.collider != null || raycastSueloDerecha.collider != null)
+        if (colisionSuelo != null)
         {
             enSuelo = true;
+            ejecutandoCoyote = false;
         }
-        else
+        else if (enSuelo && !ejecutandoCoyote)
         {
             StartCoroutine(coyotetime());
         }
 
-
         if (enSuelo && Input.GetKeyDown(KeyCode.Space))
         {
             rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, FuerzaSalto);
+            enSuelo = false;
         }
     }
+
     IEnumerator coyotetime()
     {
+        ejecutandoCoyote = true;
         yield return new WaitForSeconds(0.2f);
-        enSuelo = false;
+        if (ejecutandoCoyote)
+        {
+            enSuelo = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 posicionCaja = (Vector2)transform.position + new Vector2(0, desplazamientoY);
+        Gizmos.DrawWireCube(posicionCaja, dimensionesCaja);
     }
 }
